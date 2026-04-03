4aa219883118a5be1883adb65f625404a089a12e
mg0009 <bdbdhdbgdg@gmail.com>
2026-04-03 21:58:14 +0530

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const app = express();
 
-// current value (yahi tum control karoge)
-let SMID = "B53e1f1b8-5e3b-4c6d-9a41-b8f2d9e4c122";
+let SMID = "B53e1f1b8-5e3b-4c6d-9a41-b8f2d6e4c822";
 
-// GET → app isko call karegi
 app.get("/get-smid", (req, res) => {
   res.send(SMID);
 });
 
-// OPTIONAL: browser se value change karne ke liye
 app.get("/set-smid", (req, res) => {
   const newValue = req.query.value;
   if (newValue) {
     SMID = newValue;
     res.send("Updated: " + SMID);
   } else {
-    res.send("No value provided");
+    res.send("No value");
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+app.listen(PORT, () => console.log("Server running"));
